4a7f8b99421c8cff9b105934947d9484cd65bd33
uchihaNayeem <uchiha.nayeem@gmail.com>
2020-10-20 20:35:59 +0600

    dashBoard

--- src/components/Home/Services/Services.js
+++ src/components/Home/Services/Services.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Services.css'
 
 const Services = ({service}) => {
   return (
     
     <div className="col-md-4 single-service mb-5 text-center p-3">
             
-            <Link to={`/dashboard`}><img className="img-fluid w-25" src={service.img} alt="" /></Link>
+            <Link to={`/dashboard`}><img className="img-fluid w-25 rotate" src={service.img} alt="" /></Link>
           
             <Link to={`/dashboard`}><h3>{service.name}</h3></Link>
             <p>{service.description}</p>
